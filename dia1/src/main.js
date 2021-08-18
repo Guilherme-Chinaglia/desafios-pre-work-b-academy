ebc03c412449ba28f1d02504d8c9a7511aed8d5f
Guilherme Chinaglia <quinalia.guilherme@gmail.com>
2021-08-17 22:37:33 -0300

    Implements the visibility functionality

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,18 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+const appContent = document.querySelector("#app");
+
+appContent.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+/*Exercício 4*/
+
+const visibilityButton = document.querySelector("[data-js=link-visibilidade]");
+
+
+visibilityButton.addEventListener('click', (e)=>{
+  e.preventDefault();
+  appContent.classList.toggle('hide');
+});
